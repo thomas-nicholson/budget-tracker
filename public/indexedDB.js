2cf7e7d476da786eb660737920df5e74d7451742
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-11 10:13:52 +1000

    Added on upgrade needed method to indexed DB code

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -3,6 +3,13 @@ let db;
 // We request a database instance.
 const request = indexedDB.open("firstDatabase", 1);
 
+request.onupgradeneeded = ({ target }) => {
+    db = target.result;
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
+};
+
 request.onsuccess = ({target}) => {
     console.log("Success");
     db = target.result
